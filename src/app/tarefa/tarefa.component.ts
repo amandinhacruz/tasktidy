8c53cf25795156029c4aad34a7cb64ad27085092
amandinhacruz <amandaellem2023@gmail.com>
2025-05-21 23:18:08 -0300

    projeto finalizado

--- src/app/tarefa/tarefa.component.ts
+++ src/app/tarefa/tarefa.component.ts
@@ -1,21 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tarefa',
+  imports: [CommonModule],
   templateUrl: './tarefa.component.html',
   styleUrls: ['./tarefa.component.css']
 })
 export class TarefaComponent implements OnInit {
-  listaTarefa: any[] = [];
 
+  TAREFA_KEY = 'tarefa_key'
+  listaTarefa: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
-    this.listaTarefa = [
-      { id: 0, nome: 'Lavar o carro', concluida: false },
-      { id: 1, nome: 'Ir ao mercado', concluida: true },
-      {id: 2, nome: 'Lavar roupas', concluida: false}
-    ];
+    const tarefas = localStorage.getItem(this.TAREFA_KEY)
+    if(tarefas){
+      this.listaTarefa = JSON.parse(tarefas)
+    }
+  }
+
+  adicionar(nomeTarefa: String){
+
+    if(nomeTarefa.trim().length == 0){
+      return;
+    }
+
+    const tarefaEncontrada = this.listaTarefa.find(item => item.nome.toLowerCase() == nomeTarefa.toLowerCase())
+
+    if(!tarefaEncontrada){
+      this.listaTarefa.push({id: this.listaTarefa.length, nome: nomeTarefa, concluida: false})
+      this.salvarLista()
+    }   
+  }
+
+  deletar(id: number) {
+    this.listaTarefa = this.listaTarefa.filter(item => (item.id != id) )
+    this.salvarLista()
+  }
+
+  completar(id: number) {
+    const tarefaEncontrada = this.listaTarefa.find( item => item.id == id)
+
+    if(tarefaEncontrada) {
+      tarefaEncontrada.concluida = !tarefaEncontrada.concluida
+      this.salvarLista()
+    }
+  }
+
+  private salvarLista(){
+    localStorage.setItem(this.TAREFA_KEY, JSON.stringify(this.listaTarefa))
   }
 }
 
